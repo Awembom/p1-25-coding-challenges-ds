e42e2aca80a97a6ecc1e48686328253837eeabb1
Nkimih Albert <121590386+Awembom@users.noreply.github.com>
2023-02-15 03:30:54 +0100

    Update Exercise1.py

--- src/Exercise1.py
+++ src/Exercise1.py
@@ -1,4 +1,4 @@
-for number in range(1, 11):
-    print(number)
+for number in range(1, 10):
+    print(number + 1)
 
 #using gor loop with a final condition of 11 since for prints one less than the limit
